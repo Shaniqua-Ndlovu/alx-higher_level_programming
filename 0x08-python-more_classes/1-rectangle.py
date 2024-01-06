70f5118f3e608570df922f007f5fc926d9c4ed7c
Shaniqua-Ndlovu <155802593+Shaniqua-Ndlovu@users.noreply.github.com>
2024-01-06 11:05:32 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -34,4 +34,5 @@ class Rectangle:
             raise ValueError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
